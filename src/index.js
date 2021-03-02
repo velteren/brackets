5bbd0ac5eff56a68d0b97968cab9a828bf92a78f
velteren <senior.saveliev2012@yandex.ru>
2021-03-02 17:18:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   if (str.length % 2 !==0 ) return false;
+  let tmparr =[];
+  let tmparr2 = str.split('');
+  
+  for (let i=0; i < bracketsConfig.length; i++) {
+  	tmparr[i] = String((bracketsConfig[i])[0] + (bracketsConfig[i])[1])
+  }
+  for (let i=0; i < tmparr.length; i++) {
+  	if (~str.indexOf((tmparr[i]))) {
+    let index = str.indexOf((tmparr[i]));
+    let temp = str.split('');
+    temp.splice(index,2);
+    str = temp.join('');
+ 		}
+  }
+  for (let j = 1; j < 100; j++ ) {
+  	for (let i=0; i < tmparr.length; i++) {
+  	if (~str.indexOf((tmparr[i]))) {
+    let index = str.indexOf((tmparr[i]));
+    let temp = str.split('');
+    temp.splice(index,2);
+    str = temp.join('');
+ 		}
+  }
+  
+  }
+ 
+  if (str.length == 0) return true;
+  return false;
 }
